cd8520e316e42c7ac317ddb1cabbad1e6c86a61f
JandreiSS <jandreisst@gmail.com>
2020-05-20 14:28:58 -0300

    chore: TypeORM Repository

--- src/database/migrations/1589900172713-CreateAppointments.ts
+++ src/database/migrations/1589900172713-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1589900172713 implements MigrationInterfa
               type: 'varchar',
               isPrimary: true,
               generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
             },
             {
               name: 'provider',
