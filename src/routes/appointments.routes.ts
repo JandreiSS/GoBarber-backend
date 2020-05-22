5b8d310ac3a5de519541d7a26228abeb89847d11
JandreiSS <jandreisst@gmail.com>
2020-05-22 12:08:28 -0300

    chore: Creating records

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -12,17 +12,20 @@ appointmentsRouter.get('/', async (request, response) => {
   const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
-})
+});
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date)
 
     const createAppointment = new CreateAppointmentService()
 
-    const appointment = await createAppointment.execute({ date: parsedDate, provider })
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
 
     return response.json(appointment);
   } catch (err) {
