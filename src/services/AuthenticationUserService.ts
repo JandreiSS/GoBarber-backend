93f0ba9f95c88e3a77c5a50ddc516439c2298e07
JandreiSS <jandreisst@gmail.com>
2020-05-25 12:42:03 -0300

    chore: JWT

--- src/services/AuthenticationUserService.ts
+++ src/services/AuthenticationUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,9 +32,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    // Usuário autenticado
+    const token = sign({}, '5eae3b3e200d2cca2f45e2c07a264e59', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
