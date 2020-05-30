8aca3c906333318d6592c2f60f8a6be93895daeb
JandreiSS <jandreisst@gmail.com>
2020-05-30 16:39:39 -0300

    code-fix: Rename files

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '../services/AuthenticationUserService';
+import AuthenticateUserService from '../services/AuthenticateUserService';
 
 const sessionsRouter = Router();
 
