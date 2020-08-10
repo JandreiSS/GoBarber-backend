c7688280d8e01dee801a619f688666003616e455
JandreiSS <jandreisst@gmail.com>
2020-08-10 14:55:40 -0300

    chore: Dependency Inversion Principle

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,23 +1,23 @@
 import { startOfHour } from 'date-fns'
-import { getCustomRepository } from 'typeorm'
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsReporitory'
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface Request {
-  provider_id: string
-  date: Date
+interface IRequest {
+  provider_id: string;
+  date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  constructor(
+    private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -25,7 +25,7 @@ class CreateAppointmentService {
       throw new AppError('This appointment is alredy booked');
     };
 
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
@@ -34,4 +34,4 @@ class CreateAppointmentService {
   }
 }
 
-export default CreateAppointmentService
+export default CreateAppointmentService;
